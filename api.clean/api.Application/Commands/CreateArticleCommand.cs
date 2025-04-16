5d1c2ababe9d76a2699adc027644d6d5630922ec
SoRa <sonamraisx007@gmail.com>
2025-04-16 15:59:12 +0545

    CreateArticleCommand x ArticleResponse x CreateCommandHandler

---
+++ api.clean/api.Application/Commands/CreateArticleCommand.cs
@@ -0,0 +1,24 @@
+﻿using api.Application.Responses;
+using api.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Application.Commands
+{
+    public class CreateArticleCommand : IRequest<ArticleResponse>
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public DateTime PublishedAt { get; set; }
+        public string? ImageUrl { get; set; } = string.Empty;
+        public float SentimentScore { get; set; }
+        // foreign key
+        public int FeedSourceId { get; set; }
+    }
+}
