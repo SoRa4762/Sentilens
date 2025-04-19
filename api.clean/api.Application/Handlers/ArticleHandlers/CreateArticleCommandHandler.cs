68f3b1d4977a8d98c1e5d988c3fb27f6eed089e8
SoRa <sonamraisx007@gmail.com>
2025-04-19 15:59:31 +0545

    optimizing api: more to go

---
+++ api.clean/api.Application/Handlers/ArticleHandlers/CreateArticleCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using api.Application.Commands;
+using api.Application.Responses;
+using api.Core.Entities;
+using api.Core.Interfaces;
+using api.Application.Mappers;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Application.Handlers.ArticleHandlers
+{
+    public class CreateArticleCommandHandler : IRequestHandler<CreateArticleCommand, ArticleResponse>
+    {
+        private protected readonly IArticleRepository _articleRepository;
+        public CreateArticleCommandHandler(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
+
+        public async Task<ArticleResponse> Handle(CreateArticleCommand request, CancellationToken cancellationToken)
+        {
+            var articleEntity = ArticleMapper.Mapper.Map<Article>(request);
+            if(articleEntity == null)
+            {
+                throw new ApplicationException("Mapping failed");
+            }
+
+            var newArticle = await _articleRepository.AddAsync(articleEntity);
+            var articleResponse = ArticleMapper.Mapper.Map<ArticleResponse>(newArticle);
+            return articleResponse;
+        }
+
+    }
+}
