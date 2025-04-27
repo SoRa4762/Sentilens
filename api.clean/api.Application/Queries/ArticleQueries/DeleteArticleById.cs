1db8911c47cceb5dba8d01d2166f0ce2045296e3
SoRa <sonamraisx007@gmail.com>
2025-04-27 11:40:40 +0545

    Work: On Going - DELETE

---
+++ api.clean/api.Application/Queries/ArticleQueries/DeleteArticleById.cs
@@ -0,0 +1,19 @@
+﻿using api.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Application.Queries.ArticleQueries
+{
+    public class DeleteArticleById : IRequest<ArticleResponse>
+    {
+        public int ArticleId { get; set; }
+        public DeleteArticleById(int articleId)
+        {
+            ArticleId = articleId;
+        }
+    }
+}
